4155a7c183153e755a98dba7d2eaf60e07b2115c
adesoji <adesoji@blick.ng>
2021-09-01 11:20:16 -0600

    refactored code

---
+++ cleanup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Destroy Terraform infrastructure
+terraform destroy --auto-approve
+
+# Deregister AMI
+
+# Delete Snapshots
--- infrastructure.sh
+++ infrastructure.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
 # Build relevant AMIs
-cd /PackerAMI
+cd PackerAMI/
 packer build .
 
+# Build Infrastructure
 cd ..
 terraform init
 terraform apply --auto-approve
+
+# # Run Ansible Task
+# ssh ec2-user@[tool-server-ip]
+# sudo su - Ansible
+# git clone https://github.com/adesoji-blick/ProjectEnhancement.git
+# cd ProjectEnhancement/AnsibleTASK
+# sh "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i inventory/myhosts playbooks/playbook.yml"
