fc4a9e333a90f63c3a12abbaae0065a6bd5bf7d9
adesoji <adesoji@blick.ng>
2021-09-04 11:38:44 -0600

    refactored code

--- AnsibleTASK/ansible_dep.sh
+++ AnsibleTASK/ansible_dep.sh
@@ -4,13 +4,6 @@
 # echo setting ansible user
 # sudo su - ansible
 
-# export environment variables
-echo ----- exporting environment variables -----
-export AWS_ACCESS_KEY_ID='xxxxxxxx'
-export AWS_SECRET_ACCESS_KEY='xxxxxxxx'
-export ANSIBLE_HOSTS=/inventory/ec2.py
-export EC2_INI_PATH=/inventory/ec2.ini
-
 echo ----- installing dependencies pip -----
 # Prepare Ansible dynamic inventory
 sudo amazon-linux-extras install epel
@@ -22,8 +15,15 @@ python get-pip.py
 echo ----- installing dependencies boto -----
 pip install boto
 
-echo ----- downloading repository -----
+# export environment variables
+echo ----- exporting environment variables -----
+# export AWS_ACCESS_KEY_ID='xxxxxxxx'
+# export AWS_SECRET_ACCESS_KEY='xxxxxxxx'
+export ANSIBLE_HOSTS=/inventory/ec2.py
+export EC2_INI_PATH=/inventory/ec2.ini
+
 # download AnsibleTASK files from github
+echo ----- downloading repository -----
 git clone https://github.com/adesoji-blick/ProjectEnhancement.git
 
 cd ProjectEnhancement/AnsibleTASK/
